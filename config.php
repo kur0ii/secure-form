9c5f1c3d68a2a7cad5e93fa933af1f603a35b5cd
Michael D <126715046+kur0ii@users.noreply.github.com>
2024-02-08 23:58:11 +0100

    Update config.php

--- config.php
+++ config.php
@@ -6,10 +6,4 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root2');
 define('DB_PASSWORD', '7reHfW6*R@g!gDQ6');
 define('DB_NAME', 'users');
-
-// id INT 
-// username VARCHAR(50)
-// pwd VARCHAR(150)
-// perm INT 
-// creation DATETIME
 ?>
