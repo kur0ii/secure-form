f342ab0de36396cf7694f00a1a905241c7c6ded6
kur0ii <michael.diopr@gmail.com>
2024-02-15 21:14:35 +0100

    Update config

--- config.php
+++ config.php
@@ -1,9 +1,13 @@
 <?php
-// Identifiant de connexion de la base de donnée
-// A modifier mettre vos identifiants
+// Variables globales
 
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root2');
 define('DB_PASSWORD', '7reHfW6*R@g!gDQ6');
 define('DB_NAME', 'users');
+define('LOCK_DURATION',600);
+define('MAX_ATTEMPTS',4);
+
+
+
 ?>
